3bb530f57faf2c68c3153f4da8242bb02d3b3a51
Laszlo Lazuer <visualjazz@gmail.com>
2024-05-25 16:25:56 -0600

    feat: abstract page content with jinja2, add global templates for reusable sections.

--- todolist_app/views.py
+++ todolist_app/views.py
@@ -4,6 +4,18 @@ from django.http import HttpResponse
 # Create your views here.
 def todolist(request):
     context = {
-        'welcome_text':"Welcome from Jinja2."
+        'welcome_text':"Welcome todo list page."
         }
     return render(request, 'todolist.html', context)
+
+def contact(request):
+    context = {
+        'contact_text':"Welcome to the contact page."
+        }
+    return render(request, 'contact.html', context)
+
+def about(request):
+    context = {
+        'about_text':"Welcome to the about page."
+        }
+    return render(request, 'about.html', context)
