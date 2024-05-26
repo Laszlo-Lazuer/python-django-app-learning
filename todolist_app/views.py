678ac7518b8f0dad3e60ac57d9540668851ab230
Laszlo Lazuer <visualjazz@gmail.com>
2024-05-26 00:44:35 -0600

    chore: add edit functionality.

--- todolist_app/views.py
+++ todolist_app/views.py
@@ -24,6 +24,21 @@ def delete_task(request, task_id):
     task.delete()
     return redirect('todolist')
 
+def edit_task(request, task_id):
+    if request.method == "POST":
+        task = TaskList.objects.get(pk=task_id)
+        form = TaskForm(request.POST or None, instance = task)
+        if form.is_valid():
+            form.save()
+        
+        messages.success(request, ("Task Edited!"))    
+        return redirect('todolist')
+    else:    
+    # Fetch data from model
+        task_object = TaskList.objects.get(pk=task_id)
+    return render(request, 'edit.html', {'task_obj': task_object})
+
+
 def contact(request):
     context = {
         'contact_text':"Welcome to the contact page."
